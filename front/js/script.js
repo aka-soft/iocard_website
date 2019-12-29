43cdf88f80dbd16a54e3603d2594dfa0cfd1a028
khashayar panbeian <kpanbeian@gmail.com>
2019-12-29 18:25:38 +0330

    responsive

---
+++ front/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".navopener").click(function(){
+        $(".hd-box nav").slideToggle();
+    });
+});
+$(document).ready(function(){
+	$('.navopener').click(function(){
+		$('.navopener .fa-bars').toggleClass('fa-window-close');
+	});
+});
